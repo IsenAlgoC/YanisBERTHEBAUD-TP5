c3ab4b7482bd0155efadb2ecfe4113b553b96df3
Yanis BERTHEBAUD <yanis.berthebaud@isen.yncrea.fr>
2020-11-18 15:21:21 +0100

    exo 1 fini

--- TP5/EXO 1/EXO1.c
+++ TP5/EXO 1/EXO1.c
@@ -3,13 +3,26 @@
 #include <math.h>
 #include "tab.h"
 #define tabsize 10
-
+#define TAILLEINITIALE 100
 
 int main() {
-	int myTab1[tabsize];
-	initTab(myTab1, tabsize);
-	afficheTab(myTab1, tabsize, 2);
+	
+	int* myTab2 = NULL;
+	int TAB2SIZE = TAILLEINITIALE;
+	int nbElts = 0;
+	
+	myTab2 = (int*)malloc(TAILLEINITIALE * sizeof(int));
+	
+	if (myTab2 != NULL) { initTab(myTab2, TAB2SIZE); }
+	
+	else { printf("mémoire insuffisante"); return(-1); }
+	
+	for (int i = 0; i < 20; i++) {   //on remplie les 20 premières valeurs du tableau
+		*(myTab2 + i) = i + 1;
+	}
 
+	afficheTab(myTab2, TAB2SIZE,20);
+	free(myTab2);
 
 
 }
