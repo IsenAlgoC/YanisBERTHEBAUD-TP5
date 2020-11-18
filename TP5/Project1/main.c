586700ec9fc313e03b418fc1b4deeea8e0ddf8e4
Yanis BERTHEBAUD <yanis.berthebaud@isen.yncrea.fr>
2020-11-18 18:09:06 +0100

    fin de l'exercice 1

--- TP5/Project1/main.c
+++ TP5/Project1/main.c
@@ -1,31 +1,27 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
 #include "tab.h"
-#define tabsize 10
-#define TAILLEINITIALE 100
-#define TAILLEAJOUT 50
+
+
 
 int main() {
-	
+
 	int* myTab2 = NULL;
 	int TAB2SIZE = TAILLEINITIALE;
 	int nbElts = 100;
-	
-	
+
+
 	myTab2 = (int*)malloc(TAILLEINITIALE * sizeof(int));
-	
+
 	if (myTab2 != NULL) { initTab(myTab2, TAB2SIZE); }
-	
+
 	else { printf("mémoire insuffisante"); return(-1); }
-	
+
 	for (int i = 0; i < nbElts; i++) {   //on remplie les 20 premières valeurs du tableau
 		*(myTab2 + i) = i + 1;
 	}
-	ajoutElementDansTableau(myTab2, &TAB2SIZE, &nbElts, 101);
-	afficheTab(myTab2, TAB2SIZE, 100);
-	
-	
+	myTab2 = ajoutElementDansTableau(myTab2, &TAB2SIZE, &nbElts, 101);
+	afficheTab(myTab2, TAB2SIZE, 101);
+
+
 	free(myTab2);
 
 	return EXIT_SUCCESS;
--- TP5/Project1/tab.h
+++ TP5/Project1/tab.h
@@ -1,5 +1,9 @@
+#pragma once
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#define tabsize 10
+#define TAILLEINITIALE 100
 
 int initTab(int*, int);
 
